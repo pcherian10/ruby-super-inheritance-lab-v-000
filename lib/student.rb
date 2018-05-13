296474b6ea957f5c3852c65b33caa25dfb5c12e3
Paul <pcherian10@gmail.com>
2018-05-13 21:04:10 +0000

    Done.

---
+++ lib/student.rb
@@ -0,0 +1,13 @@
+class Student
+  attr_reader :hello, :raise_hand
+
+  def hello
+    puts "Hey there! I'm so excited to learn stuff."
+  end
+
+  def raise_hand
+    puts "Pick me!"
+  end
+
+
+end
