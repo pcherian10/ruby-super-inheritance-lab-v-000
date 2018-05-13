d78556fcb264039791f2f9cc5478fa437624a7db
Paul <pcherian10@gmail.com>
2018-05-13 21:00:48 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,11 +2,11 @@ class Student
   attr_reader :hello, :raise_hand
 
   def hello
-    "Hey there! I'm so excited to learn stuff."
+    puts "Hey there! I'm so excited to learn stuff."
   end
 
   def raise_hand
-    "Pick me!"
+    puts "Pick me!"
   end
 
 
